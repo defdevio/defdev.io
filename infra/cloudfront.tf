8fb3f2b2b9aeaae7b54ed988899079d520559e33
Tony Owens <anthony.owens1987@gmail.com>
2026-05-23 15:50:58 -0700

    Fixed depends on for new cloudflare record name

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -3,7 +3,7 @@
 # in cloudflare
 resource "time_sleep" "wait_5_minutes" {
   create_duration = "5m"
-  depends_on      = [cloudflare_record.www_defdev_io_acm_validation]
+  depends_on      = [cloudflare_dns_record.www_defdev_io_acm_validation]
 }
 
 module "cloudfront" {
