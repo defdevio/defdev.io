0af56500e2071b26290e0d47a2c32998202898a7
Tony Owens <anthony.owens1987@gmail.com>
2026-09-02 12:40:56 -0700

    Add Lambda container configuration inputs

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,8 +4,11 @@ variable "lambda_functions" {
 
   type = map(object({
     spec = object({
-      description = string
-      timeout     = optional(number, 5)
+      description           = string
+      timeout               = optional(number, 5)
+      environment_variables = optional(map(string), {})
+      entry_point           = optional(list(string))
+      command               = optional(list(string))
       ecr = object({
         is_immutable = optional(bool, false)
         image_tag    = string
